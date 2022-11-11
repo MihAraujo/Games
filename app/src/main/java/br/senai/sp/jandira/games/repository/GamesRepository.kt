2e616aac33cb2c074709cd2c8273fbe3110ad7eb
MihAraujo <milenaasouza926@gmail.com>
2022-11-11 11:25:14 -0300

    upload de arquivos

---
+++ app/src/main/java/br/senai/sp/jandira/games/repository/GamesRepository.kt
@@ -0,0 +1,9 @@
+package br.senai.sp.jandira.games.repository
+
+import android.content.Context
+
+class GamesRepository(context: Context) {
+
+
+
+}
---
+++ app/src/main/java/br/senai/sp/jandira/games/ui/SignUpActivity.kt
@@ -0,0 +1,16 @@
+package br.senai.sp.jandira.games.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.senai.sp.jandira.games.databinding.ActivityMainBinding
+import br.senai.sp.jandira.games.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySignUpBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
